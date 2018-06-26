63a6bff58e11618f2452692116fe3a9d6eeed8e9
unknown <panumas08102541@gmail.com>
2018-06-26 15:07:30 +0700

    Test upload

---
+++ src/lab1/myCode.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class myCode {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("ภาณุมาส ชัยโชติ");
+        System.out.println("360411760008");
+        System.out.println("MIT421");
+        System.out.println("มหาวิทยาลัยเทคโนโลยีราชมงคลศรีวิชัย ไสใหญ่");
+
+    }
+}
