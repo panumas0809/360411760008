00c22349be56219ca29e7202eda2c28b8ca6b544
panumas <panumas08102541@gmail.com>
2018-07-03 13:59:01 +0700

    test git account

--- src/lab1/myCode2.java
+++ src/lab1/myCode2.java
@@ -4,6 +4,8 @@ public class myCode2 {
     //main
     public static void main(String[] args) {
         System.out.println("panumas081025412gmail.com");
-
+System.out.println("kiw");
     }
 }
+
+Sys
