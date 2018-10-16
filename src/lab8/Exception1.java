fd9add6a587a2734f5a2f9bfeeb4ad02eb158af7
panumas <panumas08102541@gmail.com>
2018-10-16 14:03:01 +0700

    Exception1

---
+++ src/lab8/Exception1.java
@@ -0,0 +1,38 @@
+package lab8;
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exception1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number[] = {10, 20, 30, 40, 50};
+
+        try {
+            System.out.print("Enter an integer: ");
+            int x = sc.nextInt();
+            System.out.println("integer:" + x);
+            try {
+                System.out.println("number [" + x + "] :" + number[x]);
+            } catch (IndexOutOfBoundsException ex) {
+                System.out.println("Exception:" + ex);
+            }finally {
+                number[0] = 100;
+                System.out.println(number[0]);
+
+            }
+
+
+        } catch (InputMismatchException ex) {
+            System.out.println("Exception");
+        }
+        System.out.println("Complete.");
+
+
+
+    }//main
+
+
+
+}//class
